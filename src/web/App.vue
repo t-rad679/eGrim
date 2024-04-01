d79c61e334a1f4ade2ae9bb7e0c7ab5ac0f78264
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-03-31 23:28:23 -0400

    Move nav bar to its own component

--- src/web/App.vue
+++ src/web/App.vue
@@ -1,33 +1,10 @@
 <script setup lang="ts">
-import { Routes, routes } from "@/router/routes"
+import NavBar from "@/components/NavBar.vue"
 </script>
 
 <template>
   <v-app>
-    <v-app-bar>
-      <v-row class="text-right">
-        <v-list nav>
-          <v-col>
-            <router-link
-              :to="Routes.REGISTER"
-              tag="v-btn"
-            >
-              <v-btn>
-                Register
-              </v-btn>
-            </router-link>
-            <router-link
-              :to="Routes.LOGIN"
-              tag="v-btn"
-            >
-              <v-btn>
-                Login
-              </v-btn>
-            </router-link>
-          </v-col>
-        </v-list>
-      </v-row>
-    </v-app-bar>
+    <NavBar />
     <v-main>
       <router-view />
     </v-main>

d9aef5e3907a58d561dc798fffba15db85cb36d9
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-03-31 21:48:38 -0400

    Install vuetify

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -2,13 +2,17 @@ import path from "path"
 
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
+import vuetify from "vite-plugin-vuetify"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vuetify({ autoImport: true }),
+    ],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@": path.resolve(__dirname, "."),
         },
     },
     cacheDir: "../../node_modules",

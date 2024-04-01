38b539033942916ef1d5c582011e0b04d807430f
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-03-31 23:37:23 -0400

    Make NavBar prettier

--- src/web/components/NavBar.vue
+++ src/web/components/NavBar.vue
@@ -5,23 +5,30 @@ import { Routes } from "@/router/routes"
 
 <template>
   <v-app-bar>
-    <v-list nav>
-      <router-link
-        :to="Routes.REGISTER"
-        tag="v-btn"
-      >
-        <v-btn>
-          Register
-        </v-btn>
-      </router-link>
-      <router-link
-        :to="Routes.LOGIN"
-        tag="v-btn"
-      >
-        <v-btn>
-          Login
-        </v-btn>
-      </router-link>
-    </v-list>
+    <v-row class="text-right">
+      <v-col class="text-left">
+        <h1>
+          eGrim
+        </h1>
+      </v-col>
+      <v-col>
+        <router-link
+          :to="Routes.REGISTER"
+          tag="v-btn"
+        >
+          <v-btn>
+            Register
+          </v-btn>
+        </router-link>
+        <router-link
+          :to="Routes.LOGIN"
+          tag="v-btn"
+        >
+          <v-btn>
+            Login
+          </v-btn>
+        </router-link>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>

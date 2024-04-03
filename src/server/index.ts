bc7df9fdbd1c9639304babd4b75640ea2274b2e8
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-04-02 22:52:55 -0400

    Add functionality to generate types for use by the client

--- src/server/index.ts
+++ src/server/index.ts
@@ -32,6 +32,11 @@ async function main() {
         validate: false,
         globalMiddlewares: [PopulateUser],
     })
+
+    if(process.argv.includes("--generate_only")) {
+        console.log("Generated schema. Exiting.")
+        return
+    }
     const prisma = new PrismaClient()
     await prisma.$connect()
     const app = express()

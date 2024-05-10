1e13eebc397b8714b9a6c237be730d43e71d48f8
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-05-10 01:49:44 -0400

    Add all generated resolvers
    We had to do this because we weren't getting all the input etc types without some resolvers. Not exactly sure the relationship. We also had to add a dummy field to Event to get the schema to generate. Not sure why that is either.

---
+++ src/server/prisma/migrations/20240510054025_add_dummy_to_event/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "dummy" INTEGER;
+
+-- AlterTable
+ALTER TABLE "TagToObjectRelation" ADD COLUMN     "dummy" INTEGER;
+
+-- AlterTable
+ALTER TABLE "TarotReading" ADD COLUMN     "dummy" INTEGER;
+
+-- AlterTable
+ALTER TABLE "ToolToRitualRelation" ADD COLUMN     "dummy" INTEGER;
+
+-- AlterTable
+ALTER TABLE "WielderPersonToToolRelationRelation" ADD COLUMN     "dummy" INTEGER;

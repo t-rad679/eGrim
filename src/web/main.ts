fb98fe8273f707e043c82c64cfce9949e1ecb141
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-09-11 01:02:56 -0400

    Add credentials
    Apparently I wasn't including credentials in either direction. Fix it.

--- src/web/main.ts
+++ src/web/main.ts
@@ -14,6 +14,7 @@ import router from "@/router/router"
 
 const httpLink = createHttpLink({
     uri: "http://localhost:4000/graphql",
+    credentials: "include",
 })
 
 const cache = new InMemoryCache()

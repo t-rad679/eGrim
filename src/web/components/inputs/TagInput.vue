35ae61b539b6c33c1c2539a9d40ea1a8f21c4cbf
Trevor Radcliffe <trevor.radcliffe@gmail.com>
2024-09-11 01:03:19 -0400

    Implement tags with a store

--- src/web/components/inputs/TagInput.vue
+++ src/web/components/inputs/TagInput.vue
@@ -1,74 +1,31 @@
 <script setup lang="ts">
-import { useMutation, useQuery } from "@vue/apollo-composable"
-import { gql } from "graphql-tag"
-import { computed, ref } from "vue"
+import { computed, ref, watch } from "vue"
 
-import { useUserStore } from "@/stores/UserStore.js"
+import { useTagStore } from "@/stores/TagStore.js"
 
-const fetchTags = gql`
-  query fetchTags {
-    tags {
-      name
-    }
-  }
-`
-
-const userStore = useUserStore()
-const user = computed(() => userStore.user)
-
-const createTagMutationText = gql`
-  mutation createTag($input: TagCreateInput!) {
-    createOneTag(data: $input) {
-      name
-    }
-  }
-`
-const { result: tagsResult } = useQuery(fetchTags)
-const { mutate: createTagMutation, onDone: createTagOnDone, onError: createTagOnError } = useMutation(createTagMutationText)
+const tagStore = useTagStore()
+const availableTags = computed(() => tagStore.tags.map((tag) => tag.name))
 const selectedTags = ref([])
 const search = ref("")
-const error = ref("")
-const availableTags = computed(() => {
-    return tagsResult.value?.tags.filter(tag =>
-        !selectedTags.value.some(selectedTag => selectedTag.id === tag.id),
-    ) || []
-})
-const addTag = (tag) => {
-    selectedTags.value.push(tag)
-}
-const createTag = async (tagName) => {
-    console.log(tagName)
-    createTagOnDone((data) => {
-        addTag(data.createTag)
-    })
-    createTagOnError((createTagError) => {
-        console.error(createTagError)
-        error.value = createTagError.message
-    })
-    await createTagMutation({
-        input: {
-            name: tagName,
-            user: {
-                connect: {
-                    username: user.value.name,
-                },
-            },
-        },
-    })
-}
-const onTagSelected = (tagName) => {
-    const existingTag = availableTags.value.find(tag => tag.name === tagName)
-    if (existingTag) {
-        addTag(existingTag)
-    } else {
-        createTag(tagName)
+const error = computed(() => tagStore.error)
+
+watch(selectedTags, (newSelectedTags, oldSelectedTags) => {
+    const newlySelectedTags = newSelectedTags.filter((tagName) => !oldSelectedTags.includes(tagName))
+    const tagsToCreate = newlySelectedTags.filter((tagName) => !availableTags.value.includes(tagName))
+    if (tagsToCreate.length > 0) {
+        tagStore.createTags(tagsToCreate)
+        if (tagStore.error) {
+            return
+        }
     }
-}
+    search.value = ""
+})
+
 </script>
 <template>
   <v-combobox
     v-model:search="search"
-    v-model:selected-items="selectedTags"
+    v-model="selectedTags"
     :hide-no-data="false"
     auto-select-first
     clear-on-select
@@ -77,7 +34,6 @@ const onTagSelected = (tagName) => {
     label="Tags"
     multiple
     :items="availableTags"
-    @update:model-value="onTagSelected"
   >
     <template #no-data>
       <v-list-item>
